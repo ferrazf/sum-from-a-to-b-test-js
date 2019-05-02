6d07864aacb8b1a6032f0d0a2fa0119c246e1ca0
ferrazf <ferrazf9@gmail.com>
2019-05-02 22:36:15 +0000

    Recursevly sum all the values from fromN up to toN

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-// Sum all the values from fromN up to toN
+// Recursevly sum all the values from fromN up to toN
 function sum(fromN, toN) {
   if (toN === fromN) return fromN;
   return sum(fromN, toN - 1) + toN;
